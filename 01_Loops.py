770ef541bb21be70ba13f1c5b02dc8ee4957eb03
Ganesh Malepati <97588943+ganeshmalepati@users.noreply.github.com>
2024-04-03 16:10:10 +0530

    Add files via upload

---
+++ 01_Loops.py
@@ -0,0 +1,15 @@
+num=9
+for i in range(10):
+    print(f"{num} * {i+1} = {num * (i+1)}")
+for i in range(5):
+    print(f"Hey my age is {num}")  #Formating function in python 
+
+Number=int(input("Enter a number: "))
+for i in range(2, Number):
+    if(Number%2==0):
+        print("Not a prime number")
+        break
+else:
+    print("Prime number")
+
+    
